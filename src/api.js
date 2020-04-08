b03046c053c6e2ca73b395fb1ebbf59db06dda60
Roman Belov <rnbelov96@gmail.com>
2020-04-08 16:45:34 +0300

    api url изменен

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://roh7771-reagent-api.glitch.me/api/v1',
+  baseURL: 'https://reagent-api.herokuapp.com/api/v1',
   timeout: 30000,
   withCredentials: true,
 });
